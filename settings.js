4b1703d5dea4f8ad0a6192004fee5077749d80e3
safwanueser <safwangoat607@gmail.com>
2025-02-21 20:50:08 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -7,7 +7,7 @@ function convertToBool(text, fault = 'true') {
 module.exports = {
 //👉Put Your Session Id👈 කියන එක කපලා අයින් කරලා ඔයාගේ Session id එක දාන්න.
 
-SESSION_ID: process.env.SESSION_ID || "Put Your Session Id",
+SESSION_ID: process.env.SESSION_ID || "LAKA-MD=Yo4SSBwJ#tVFQEmMw0wvCTCPE_hQvIspJzVBDt3M04Nid6vq8wwY",
 
 
 //=================================================
